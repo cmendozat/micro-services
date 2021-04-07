61223353f91b691a2f3b4e7c410470fef3e915bf
cmendoza <carlos-jose0294@hotmail.com>
2021-04-06 22:13:48 -0500

    add getmutant micro-service

---
+++ getmutant/src/main/java/co/com/getmutant/getmutant/GetmutantApplication.java
@@ -0,0 +1,13 @@
+package co.com.getmutant.getmutant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmutantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmutantApplication.class, args);
+	}
+
+}
---
+++ getmutant/src/test/java/co/com/getmutant/getmutant/GetmutantApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.getmutant.getmutant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetmutantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
