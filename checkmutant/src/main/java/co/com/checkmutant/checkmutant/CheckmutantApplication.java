c48ddbe372f065ab13bc773d9c099cbeb97f2336
cmendoza <carlos-jose0294@hotmail.com>
2021-04-06 22:02:23 -0500

    add checkmutant micro-service

---
+++ checkmutant/src/main/java/co/com/checkmutant/checkmutant/CheckmutantApplication.java
@@ -0,0 +1,13 @@
+package co.com.checkmutant.checkmutant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmutantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckmutantApplication.class, args);
+	}
+
+}
---
+++ checkmutant/src/test/java/co/com/checkmutant/checkmutant/CheckmutantApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.checkmutant.checkmutant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckmutantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
