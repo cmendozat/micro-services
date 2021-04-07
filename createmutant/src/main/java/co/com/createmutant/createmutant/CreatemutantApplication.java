2649d7948ccff79cb029f365d7ebacf8b245c2ac
cmendoza <carlos-jose0294@hotmail.com>
2021-04-06 22:11:52 -0500

    add createmutant micro-services

---
+++ createmutant/src/main/java/co/com/createmutant/createmutant/CreatemutantApplication.java
@@ -0,0 +1,13 @@
+package co.com.createmutant.createmutant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatemutantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatemutantApplication.class, args);
+	}
+
+}
---
+++ createmutant/src/test/java/co/com/createmutant/createmutant/CreatemutantApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.createmutant.createmutant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatemutantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
